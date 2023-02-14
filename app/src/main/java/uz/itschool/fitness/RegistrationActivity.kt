f262b9c1b2d5be3468a03ac7e68f2be2dd18e0d6
Khabibullokh <68428742+KHabibullokh2006@users.noreply.github.com>
2023-02-14 13:13:20 +0500

    Update RegistrationActivity.kt

--- app/src/main/java/uz/itschool/fitness/RegistrationActivity.kt
+++ app/src/main/java/uz/itschool/fitness/RegistrationActivity.kt
@@ -5,63 +5,67 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
 class RegistrationActivity : AppCompatActivity() {
 
-//    lateinit var reg
-//    lateinit var name
-//    lateinit var username
-//    lateinit var phone
-//    lateinit var email
-//    lateinit var password
-//    lateinit var rep_password
+    lateinit var reg:Button
+    lateinit var name:TextInputEditText
+    lateinit var username:TextInputEditText
+    lateinit var phone:TextInputEditText
+    lateinit var email:TextInputEditText
+    lateinit var password:TextInputEditText
+    lateinit var rep_password:TextInputEditText
+
+    private lateinit var userList:MutableList<User>
+    val shared = getSharedPreferences("login", MODE_PRIVATE)
+    val edit = shared.edit()
+    private val gson = Gson()
+    val convert = object : TypeToken<List<User>>(){}.type
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
-        var userList:MutableList<User>
-        val shared = getSharedPreferences("login", MODE_PRIVATE)
-        val edit = shared.edit()
-        val gson = Gson()
-        val convert = object : TypeToken<List<User>>(){}.type
 
-         var reg:Button = findViewById(R.id.signup_reg)
-         var name:TextInputEditText = findViewById(R.id.name)
-         var username:TextInputEditText = findViewById(R.id.username)
-         var phone:TextInputEditText = findViewById(R.id.phone)
-         var email:TextInputEditText = findViewById(R.id.mail)
-         var password:TextInputEditText = findViewById(R.id.password)
-         var rep_password:TextInputEditText = findViewById(R.id.rep_password)
+         initUI()
 
         reg.setOnClickListener {
             validate()
             val users = shared.getString("users","")
             if (users==""){
                 userList = mutableListOf()
-            }else{
+            }
+            else{
                 userList=gson.fromJson(users,convert)
             }
+
             userList.add(User(name,username,phone,email,password))
 
             val str = gson.toJson(userList)
             edit.putString("users", str).apply()
 
 
-            var intent = Intent(this,LoginActivity::class.java)
+            val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
         }
 
     }
 
-    fun initUI(){
-
+    private fun initUI(){
+        reg = findViewById(R.id.signup_reg)
+        name = findViewById(R.id.name)
+        username = findViewById(R.id.username)
+        phone = findViewById(R.id.phone)
+        email = findViewById(R.id.mail)
+        password = findViewById(R.id.password)
+        rep_password = findViewById(R.id.rep_password)
     }
     private fun validate(){
-        if(){
-
+        if(name.equals("")|| username.equals("") || phone.equals("") || email.equals("") || password.equals("") || rep_password.equals("")){
+            Toast.makeText(this,"Fill the form fully", Toast.LENGTH_SHORT).show()
         }
     }
 }
